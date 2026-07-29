40e9ce533c1729f75235e977ca1fa9836432ea58
Neha Asole <nehaasole529@gmail.com>
2026-07-29 23:31:58 +0530

    Time: 11 ms (26.96%) | Memory: 14.7 MB (11.35%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        count={}
+        for num in nums:
+            count[num]=count.get(num,0)+1
+        for num in count:
+            if count[num]==1:
+                return num    
+        
